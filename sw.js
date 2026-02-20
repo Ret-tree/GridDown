f9d9822e478affe6adab451f3247456d66ef427f
Ret-tree <cammcknig9@gmail.com>
2026-02-20 18:03:30 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'griddown-v6.57.54';
+const CACHE_NAME = 'griddown-v6.57.57';
 const TILE_CACHE_NAME = 'griddown-tiles-v1';
 const STATIC_ASSETS = [
     './', 'index.html', 'manifest.json', 'favicon.ico', 'css/app.css',
